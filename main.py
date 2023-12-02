3bf488145ca4be044ed48d8595b1ef73917de952
pastewarecode <codytran123456@icloud.com>
2023-12-01 23:41:25 -0700

    finished the letter and number count

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # context manager
 with open("mydefaults.ini.txt") as ini_file:
     data = ini_file.read()
@@ -11,3 +10,21 @@ for line in lines:
         fields = line.split("=")
         key_value_ini[fields[0]] = fields[1]
 print(key_value_ini)
+
+
+letter_counter = 0
+
+for letter in data:
+    if letter.isalpha():
+        letter_counter += 1
+
+number_counter = 0
+
+for number in data:
+    if number.isnumeric():
+        number_counter += 1
+
+with open("outputs.txt", "w") as outputs:
+    outputs.write(f"Amount of letters: {letter_counter}\n")
+    outputs.write(f"Amount of numbers: {number_counter}")
+
